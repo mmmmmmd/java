f8f6a4f5e861579a87dd7970323f55407a56d0d5
Zh.EastSun <947519418@qq.com>
2017-09-21 20:37:59 +0800

    固定长度的List

---
+++ FixedListTest/src/FixedListTest.java
@@ -0,0 +1,12 @@
+
+/*
+   Arrays工具类里提供了asList(Object... a)方法，该方法可以把一个数组或指定个数的对象转换成一个List集合
+   这个List集合既不是ArrayList的实现类的实例，也不是Vector实现类的实例，而是Arrays的内部类ArrayList的实例
+
+   Arrays.ArrayList是一个固定长度的List集合，程序只能遍历访问集合里的元素，不可增加、删除该集合里的元素。
+ */
+public class FixedListTest {
+    public static void main(String[] args){
+        
+    }
+}
