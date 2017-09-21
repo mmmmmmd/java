fbff555f7f6ac361a7bd58fc68586172ab9150cb
Zh.EastSun <947519418@qq.com>
2017-09-21 20:12:24 +0800

    练习ListIterator迭代器

--- ListIteratorTest/src/ListIteratorTest.java
+++ ListIteratorTest/src/ListIteratorTest.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 
 /*
    与Set只提供了一个iterator()方法不同，List还额外提供了一个listIterator()方法，该方法返回一个ListIterator对象，
@@ -11,4 +14,28 @@
    ListIterator可以用add()方法添加元素，而Iterator只能用remove()方法删除元素
  */
 public class ListIteratorTest {
+    public static void main(String[] args){
+        String[] books = {"白夜行","嫌疑人X的献身","解忧杂货店"};
+        List bookList = new ArrayList();
+        for(int i = 0;i<books.length;i++){
+            bookList.add(books[i]);
+        }
+
+        System.out.println("集合初始化完毕..");
+
+        ListIterator listIterator = bookList.listIterator();
+
+        //使用正向遍历
+        while (listIterator.hasNext()){
+            System.out.println(listIterator.next());
+            listIterator.add("-----分隔符-----");
+        }
+
+        System.out.println("------以下为反向迭代-----");
+
+        //使用反向遍历
+        while (listIterator.hasPrevious()){
+            System.out.println(listIterator.previous());
+        }
+    }
 }
